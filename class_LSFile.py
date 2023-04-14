60343ecea213082cff402602a958e3a4ada72ad0
Matthew Herber <32679887+Happyrobot33@users.noreply.github.com>
2023-04-14 08:10:45 -0400

    Update class_LSFile.py

--- class_LSFile.py
+++ class_LSFile.py
@@ -7,33 +7,41 @@ from class_P_Info import P_Info
 from class_P_Lines import P_Lines
 from class_P_PositionalData import P_PositionalData
 import util
+import os
 
 
 class LSFile:
     # init is called when the class is created
     # it will take in a reference to the file name of the program to be loaded
-    def __init__(self, fileName):
+    def __init__(self, filePath):
         # Store the file name
-        self.fileName = fileName
+        self.filePath = filePath
+        #get the file name from the file path
+        self.fileName = os.path.basename(self.filePath)
 
         # Load the file
         self.loadFile()
 
-        #if the file name is longer than 35 characters, trim it
-        if len(self.fileName) > 35:
-            self.fileName = self.fileName[:35]
+        #if the file name is longer than 33 characters, trim it
+        #dont ask me why, but the max file name length is 33 characters. if it is longer than that, the program will say the file already exists
+        if len(self.fileName) > 33:
+            #trim the file name to 33 characters
+            self.fileName = self.fileName[:33]
+            #trim leading/trailing whitespaces
+            self.fileName = self.fileName.strip()
+            self.filePath = os.path.dirname(self.filePath) + "/" + self.fileName
             #inform the user that the file name has been trimmed
-            print("File name trimmed to 35 characters! new file name: " + self.fileName)
+            print("File name trimmed to 33 characters! new file name: " + self.fileName)
 
         #make sure the /prog name is the same as the file name
         #remove the ToConvert/ from the file name
-        self.programInfo.Name = self.fileName[10:]
-        self.programInfo.FileName = self.fileName[10:]
+        self.programInfo.Name = self.fileName
+        self.programInfo.FileName = self.fileName
 
     # This function will load the file and split it into the three sections, to be passed to sub classes that will contain the data
     def loadFile(self):
         # Get the file contents
-        fileContents = util.getFileContents(self.fileName)
+        fileContents = util.getFileContents(self.filePath)
 
         # Split the file into the three sections
         # the first section is up to /MN, and contains the program info itself
@@ -88,5 +96,5 @@ class LSFile:
         # Return the current program
         return self
 
-    def saveFile(self, fileName):
-        util.saveFile(fileName, self.toString())
+    def saveFile(self, filePath):
+        util.saveFile(filePath, self.toString())
